d3e69baa0845198995945de2c22f4f91a6704eda
PaulMercer1 <48514185+PaulMercer1@users.noreply.github.com>
2022-06-29 13:33:12 +0100

    Update Startup.cs
    Add in code to automatically apply any migrations on startup

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -34,9 +34,20 @@ namespace eTickets
             services.AddControllersWithViews();
         }
 
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+        using var serviceScope = app.ApplicationServices
+            .GetRequiredService<IServiceScopeFactory>()
+            .CreateScope();
+        using var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+        context.Database.Migrate();
+        }
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app)
+                 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
