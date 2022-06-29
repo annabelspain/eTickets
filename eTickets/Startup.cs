1aed6f5ecc3774ee08bdf397ac308de978497b1b
PaulMercer1 <48514185+PaulMercer1@users.noreply.github.com>
2022-06-29 13:55:25 +0100

    Update Startup.cs
    Add missing semi colon

--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -46,7 +46,7 @@ namespace eTickets
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            UpdateDatabase(app)
+            UpdateDatabase(app);
                  
             if (env.IsDevelopment())
             {
